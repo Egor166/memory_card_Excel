58fe0e1ac5194ec925d68eaa374d88b4a64e7e36
Egor166 <e53737669@gmail.com>
2025-11-23 17:30:57 +0300

    V2.3

--- test.py
+++ test.py
@@ -3,5 +3,13 @@ import pandas as pd
 df = pd.read_excel('memory_card.xlsx')
 
 # Предположим, у вас есть столбец с названием "Имя"
-name = df.at[0, 'Вопросы']  # Первая строка в столбце "Имя"
-print(name)
+
+  
+name = df.at[q, 'Вопросы']  # Первая строка в столбце "Имя"
+
+column_name = 'Вопросы'  # Замените на нужное название столбца
+count_non_empty = df[column_name].count()
+
+for i in range(count_non_empty):
+    name = df.at[i, 'Вопросы']
+    print(name)
